955448dacf3594986afdff5e730e75539dbe1416
Dmitry Galashin <MustangDSG@ya.ru>
2016-03-05 17:53:21 +0300

    Added float types in Fahrenheit to Celsius converter

--- practice_001/main.c
+++ practice_001/main.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 //    }
 
     //Fahrenheit to Celsius converter
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, step;
 
     lower = -100;
@@ -30,8 +30,8 @@ int main(int argc, char *argv[])
     fahr = lower;
     while (fahr <= upper)
     {
-        celsius = 5 * (fahr-32) / 9;
-        printf("%4d\t%6d\n", fahr, celsius);
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%4.0f\t%6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 
