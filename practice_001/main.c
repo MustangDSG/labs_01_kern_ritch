8dd29abde40583308074546df2d1b43deb38a010
Dmitry Galashin <MustangDSG@ya.ru>
2016-03-05 12:46:10 +0300

    Added example with Fahrengait to Celsium conversion programm.

--- practice_001/main.c
+++ practice_001/main.c
@@ -2,17 +2,37 @@
 
 int main(int argc, char *argv[])
 {
-    //printf("Test1\n");
-    printf("Hello, World!\n");
-    //printf(*argv[argc]);
-    printf("Hello, ");
-    printf("World!");
-    printf("\n");
+    //ex. 1.1
 
-    for (volatile int i=0; i<10; i++)
+//    //printf("Test1\n");
+//    printf("Hello, World!\n");
+//    //printf(*argv[argc]);
+//    printf("Hello, ");
+//    printf("World!");
+//    printf("\n");
+
+    //ex. 1.2
+
+//    for (volatile int i=0; i<10; i++)
+//    {
+//        printf("\a");
+//        //printf("%d", i);
+//    }
+
+    int fahr, celsius;
+    int lower, upper, step;
+
+    lower = -100;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+    while (fahr <= upper)
     {
-        printf("\a");
-        //printf("%d", i);
+        celsius = 5 * (fahr-32) / 9;
+        printf("%d\t%d\n", fahr, celsius);
+        fahr = fahr + step;
     }
+
     return 0;
 }
